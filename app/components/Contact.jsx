6eca66dc2a7c591f15685da138e2977e2074f4db
tamizhasn <tamilarasanown@gmail.com>
2025-08-31 21:53:18 +0530

    bg update

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
   if (!mounted) return null;
 
   return (
-    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
+    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto] z-10'>
       <h4 className='text-center mb-2 text-lg font-Ovo'>
         Connect with me</h4>
       <h2 className='text-center text-5xl font-Ovo'>
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col 
-    items-center justify-center gap-4'>
+    items-center justify-center gap-4 relative z-10'>
       <div>
         <Image src={assets.profile_img} alt='' className='rounded-full w-64'/>
       </div>
